6ceb3a1c76cf433253a5c0c08c3d8fcf5605a2be
keenan560 <keenan560@gmail.com>
2022-03-06 19:35:55 -0600

    added .env file for client_id

--- src/App.tsx
+++ src/App.tsx
@@ -8,108 +8,152 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  TouchableOpacity,
+  TextInput,
+  FlatList,
+  Image,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import {CLIENT_ID} from '@env';
+import {CLIENT_ID} from 'react-native-dotenv';
 
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
+  interface IsSearch {
+    search: string;
+    api: string;
+  }
+
+  const [searchQuery, setSearchQuery] = useState<IsSearch['search']>('');
+  const apiEndPoint: IsSearch['api'] = `https://api.unsplash.com/search/photos?page=1&query=${searchQuery}&client_id=${CLIENT_ID}&page=1`;
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const unsplashSearch = async () => {
+    if (searchQuery) {
+      setIsLoading(true);
+      await fetch(apiEndPoint)
+        .then(response => response.json())
+        // eslint-disable-next-line @typescript-eslint/no-shadow
+        .then(data => setData(data.results));
+    } else {
+      alert('Please enter a valid query');
+    }
+  };
+
+  const clearResults = () => {
+    setData([]);
+    setSearchQuery('');
+  };
+
+  const renderItem = ({item}) => {
+    return (
+      <View style={styles.itemContainer}>
+        <TouchableOpacity onPress={() => {}}>
+          <Image source={{uri: item.urls.full}} style={styles.itemThumbnail} />
+          {/* <Text style={styles.itemDescription}>
+            {item.description ?? searchQuery}
+          </Text> */}
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <StatusBar />
+      <Text style={styles.header}>Unsplash Photo Search!</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Search for photos.."
+        value={searchQuery}
+        onChangeText={text => setSearchQuery(text)}
+        onSubmitEditing={unsplashSearch}
+      />
+      <TouchableOpacity style={styles.clearButton} onPress={clearResults}>
+        <Text style={styles.clearText}>Clear</Text>
+      </TouchableOpacity>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+          showsVerticalScrollIndicator={false}
+        />
+      </SafeAreaView>
     </View>
   );
 };
 
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: '#edeff2',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  header: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    color: '#2960df',
+    padding: 20,
+    marginTop: 50,
+    marginBottom: 15,
+    fontFamily: 'sans-serif-condensed',
   },
-  sectionDescription: {
-    marginTop: 8,
+  input: {
+    borderWidth: 0.5,
+    borderColor: '#050505',
+    borderRadius: 5,
+    width: 363.4,
+    height: 60,
+    paddingLeft: 10,
+    marginBottom: 30,
+    backgroundColor: '#fff',
+  },
+  spinner: {
+    height: 100,
+    width: 50,
+  },
+  itemContainer: {
+    borderWidth: 0.5,
+    width: 363.4,
+    padding: 15,
+    marginBottom: 20,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+  },
+  itemThumbnail: {
+    height: 300,
+    borderWidth: 10,
+    marginBottom: 5,
+    backgroundColor: '#fff',
+  },
+  itemDescription: {
+    color: 'gray',
     fontSize: 18,
-    fontWeight: '400',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 2,
+    fontStyle: 'italic',
   },
-  highlight: {
-    fontWeight: '700',
+  clearButton: {
+    backgroundColor: '#2960df',
+    width: 363.4,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+    marginBottom: 20,
+  },
+  clearText: {
+    padding: 10,
+    color: '#fff',
+    fontWeight: 'bold',
   },
 });
 
